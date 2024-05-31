7f15935b24b66df4f6a42190612cc49c60ed6dfa
huydt84 <viethuy061002@gmail.com>
2024-05-31 17:11:36 +0700

    add model inference

--- celery/app/tasks.py
+++ celery/app/tasks.py
@@ -7,14 +7,14 @@ from worker import celery
 # Tạo logger
 logger = logging.getLogger(__name__)
 
-tts_api = "http://tts_service:3000/tts"
+tts_api = "http://tts_service:3000/tts-real"
 
 requests.adapters.DEFAULT_RETRIES = 5
 
 @celery.task(name=config["CELERY_TASK"], queue='tasks', bind=True)
 def tts(self, folder_name, content):
     # Get audio
-    response = requests.post(url=tts_api, json={"content": content, "folder_name": folder_name}, timeout=120)
+    response = requests.post(url=tts_api, json={"content": content, "folder_name": folder_name})
 
     return response.json()
 
